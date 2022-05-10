d0661846f59bf829e9fa19a70629462490f272a1
PabloChiatti <96429494+PabloChiatti@users.noreply.github.com>
2022-05-10 12:26:13 -0300

    Add files via upload

---
+++ scrips/browserChanges.js
@@ -0,0 +1,23 @@
+// function BrowserDetection() {
+//     //Check if browser is Firefox 
+//      if (navigator.userAgent.search("Firefox") >= 0) {
+//         // insert conditional Firefox Code here
+//     }
+//     //Check if browser is Safari
+//     else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+//         // insert conditional Safari code here
+//     }
+//     //Check if browser is Opera
+//     else if (navigator.userAgent.search("Opera") >= 0) {
+//         // insert conditional Opera code here
+//     }
+// }
+
+// let detector = new MobileDetect(window.navigator.userAgent);
+
+// if (detector.phone() == null || detector.userAgent() == "Safari") {
+//     alert(`Mobile: ${detector.mobile()}
+//      Phone: ${detector.phone()}
+//      OS: ${detector.os()}
+//      userAgent: ${detector.userAgent()}`)
+// }
