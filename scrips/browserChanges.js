d74c98be0b7bdae53cc4dac993f748a4a14ad5ac
MrSzasz <lugoleandro@hotmail.com>
2022-05-09 22:48:55 -0300

    list

--- scrips/browserChanges.js
+++ scrips/browserChanges.js
@@ -13,11 +13,11 @@
 //     }
 // }
 
-let detector = new MobileDetect(window.navigator.userAgent);
+// let detector = new MobileDetect(window.navigator.userAgent);
 
-if (detector.phone() == null || detector.userAgent() == "Safari") {
-    alert(`Mobile: ${detector.mobile()}
-     Phone: ${detector.phone()}
-     OS: ${detector.os()}
-     userAgent: ${detector.userAgent()}`)
-}
+// if (detector.phone() == null || detector.userAgent() == "Safari") {
+//     alert(`Mobile: ${detector.mobile()}
+//      Phone: ${detector.phone()}
+//      OS: ${detector.os()}
+//      userAgent: ${detector.userAgent()}`)
+// }
