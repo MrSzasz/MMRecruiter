607dc32a94b6df2ac251f41091e62107ce9fdbd9
MrSzasz <lugoleandro@hotmail.com>
2022-05-12 18:46:41 -0300

    iphone test 3

--- scrips/browserChanges.js
+++ scrips/browserChanges.js
@@ -1,28 +1,22 @@
-// let detector = new MobileDetect(window.navigator.userAgent);
+let detector = new MobileDetect(window.navigator.userAgent);
 
-// let browserName = (function (agent) {
-//     switch (true) {
-//         case agent.indexOf("firefox") > -1:
-//             return "Mozilla Firefox";
-//         case agent.indexOf("safari") > -1:
-//             return "Safari";
-//         default:
-//             return "other";
-//     }
-// })(window.navigator.userAgent.toLowerCase());
+let browserName = (function (agent) {
+    switch (true) {
+        case agent.indexOf("firefox") > -1:
+            return "Mozilla Firefox";
+        default:
+            return "other";
+    }
+})(window.navigator.userAgent.toLowerCase());
 
-// if (detector.phone() != null) {
+if (detector.phone() != null) {
 
-//     if (detector.userAgent() != 'Chrome' && detector.userAgent() != 'Firefox') {
-//         $('.mainImg').css('background-attachment:', 'scroll', 'background-position:', 'right', 'background-size:', 'contain');
-//     } else if (detector.userAgent() == 'Firefox') {
-//         $('.hasJs').toggleClass('blur', true);
-//     }else if (detector.is('iPhone')){
-//         $('.mainImg').css('background-attachment:', 'scroll', 'background-position:', 'right', 'background-size:', 'contain');
-//     }
+    if (detector.userAgent() != 'Chrome' && detector.userAgent() != 'Firefox') {
+        $('.mainImg').css('background-attachment', 'scroll');
+    } else if (detector.userAgent() == 'Firefox') {
+        $('.hasJs').toggleClass('blur', true);
+    }
 
-// } else if (browserName == 'Mozilla Firefox') {
-//     $('.hasJs').toggleClass('blur', true);
-// } else if (browserName == 'Safari') {
-//     $('.mainImg').css('background-attachment:', 'scroll');
-// }
+} else if (browserName == 'Mozilla Firefox') {
+    $('.hasJs').toggleClass('blur', true);
+}
